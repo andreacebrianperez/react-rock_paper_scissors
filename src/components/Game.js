9860295b0500f0da2513515c6ecfdeb6f12310a8
Andrea Cebrián <andreacebrianp@gmail.com>
2022-12-20 18:03:33 +0100

    part 2 - maquetahecha a falta de detalle de texto

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
+import rock from "../img/rock.png";
+import paper from "../img/paper.png";
+import scissors from "../img/scissors.png";
 import "./Game.css";
 
 export default function Game() {
@@ -8,16 +11,25 @@ export default function Game() {
     <>
       <Container>
         <Row className="gamePartsDistribution">
-          <div className="darkBackground computerPart">
-            <h1 className="darkText textBoldXL">
-              LET'S PLAY!
-            </h1>
+          <div className="darkBackground computerPart bouncing-text">
+            <div className="l darkText textBoldXL"> L </div>
+            <div className="e darkText textBoldXL"> E </div>
+            <div className="t darkText textBoldXL"> T </div>
+            <div className="apostrophe darkText textBoldXL"> ' </div>
+            <div className="s darkText textBoldXL"> S </div>
+            <div style={{visibility: "hidden"}}> a </div>
+            <div className="p darkText textBoldXL"> P </div>
+            <div className="l2 darkText textBoldXL"> L </div>
+            <div className="a darkText textBoldXL"> A </div>
+            <div className="y darkText textBoldXL"> Y </div>
+            <div className="shadow"></div>
+            <div className="shadow-two"></div>
           </div>
 
           <div className="lightBackground playerPart">
-            <h1 className="lightText textBoldXL">LET'S PLAY</h1>
-
-            <img src="" alt=""></img>
+            <img src={rock} alt="" />
+            <img src={paper} alt="" />
+            <img src={scissors} alt="" />
           </div>
         </Row>
       </Container>
